e620cb20b8c7649384f3d3beeec8c46ce7f74c4c
AntonRm <tworkn1@gmail.com>
2023-09-29 20:51:30 +0300

    change structure

---
+++ src/GameScene.js
@@ -0,0 +1,47 @@
+import Phaser from 'phaser'
+
+const URL = 'http://localhost:5173'
+
+export default class GameScene extends Phaser.Scene {
+	constructor() {
+		super('Game')
+	}
+
+	init() { }
+
+	preload() {
+		// this.load.image('sheet', `${URL}/assets/sheet.png`)
+		// this.load.tilemapTiledJSON('map', `${URL}/assets/map.json`)
+		this.load.image('sheet', '/sheet.png')
+		this.load.tilemapTiledJSON('map', '/map.json')
+	}
+
+	create() {
+		const map = this.make.tilemap({ key: 'map' })
+		const tileset = map.addTilesetImage('sheet')
+		const background = map.createLayer('background', tileset)
+		const ships = map.createBlankLayer('ships', tileset)
+		const result = ships.putTilesAt([
+			[1, 6, 6, 6, 6, 6, 6, 6, 6, 6],
+			[6, 2, 6, 6, 6, 6, 6, 6, 6, 6],
+			[6, 6, 3, 6, 6, 6, 6, 6, 6, 6],
+			[6, 6, 6, 4, 6, 6, 6, 6, 6, 6],
+			[6, 6, 6, 6, 7, 6, 6, 6, 6, 6],
+			[6, 6, 6, 6, 6, 8, 6, 6, 6, 6],
+			[6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
+			[6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
+			[6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
+			[6, 6, 6, 6, 6, 6, 6, 6, 6, 6]
+		], 1, 1)
+
+		this.input.on('pointerup', function (pointer) {
+			const tile = map.getTileAtWorldXY(pointer.worldX, pointer.worldY)
+			console.log(tile)
+		}, this)
+	}
+
+	update(time, dt) {
+
+	}
+
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,34 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './style.css'
-import App from './App'
-import './canvas/game'
+import Phaser from 'phaser'
+import GameScene from './GameScene'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
-	<App />
-)
+const config = {
+	type: Phaser.AUTO,
+	width: 352,
+	height: 352,
+	scene: [GameScene],
+	parent: 'game',
+	scale: {
+		// mode: Phaser.Scale.FIT,
+		autoCenter: Phaser.Scale.CENTER_BOTH
+		// mode: Phaser.Scale.ScaleModes.RESIZE,
+		// width: window.innerWidth,
+		// height: window.innerHeight,
+	},
+	physics: {
+		default: 'matter',
+		matter: {
+			debug: false,
+			gravity: { x: 0, y: 0 }
+		}
+		// default: 'arcade',
+		// arcade: {
+		// 	gravity: { y: 0 },
+		// 	debug: false,
+		// }
+	}
+}
+const tg = window.Telegram.WebApp
+const game = new Phaser.Game(config)
+
+console.log(tg.initData)
+console.log(tg.initDataUnsafe)
